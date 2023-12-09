3e83b1ab488ee04d00830114128313c0cd57a2b3
Bielle-lima <141188124+Bielle-lima@users.noreply.github.com>
2023-12-09 10:41:12 -0300

    Add files via upload

---
+++ Projeto - Apresentação/Projeto - Apresentação/HTML/JS/form.js
@@ -0,0 +1,31 @@
+// Principal3.js
+
+function validateForm() {
+    // Adicione  condições de validação aqui
+    var usuario = document.getElementById('usuario').value;
+    var cpf = document.getElementById('cpf').value;
+    var dataNascimento = document.getElementById('dataNascimento').value;
+    var email = document.getElementById('email').value;
+    var contato = document.getElementById('contato').value;
+
+    // todos os campos devem ser preenchidos
+    if (usuario === '' || cpf === '' || dataNascimento === '' || email === '' || contato === '') {
+        alert('Por favor, preencha todos os campos.');
+        return false; // Impede o envio do formulário
+    }
+
+    // Adiciona um alerta indicando que o usuário foi cadastrado com sucesso
+alert('Usuário cadastrado com sucesso!');
+
+
+    return true; // Permite o envio do formulário se todas as condições forem atendidas
+}
+
+function onMouseOverForm() {
+    console.log("Mouse sobre o formulário!");
+}
+
+
+function onEmailChange() {
+    console.log("E-mail alterado:", document.querySelector('input[type="email"]').value);
+}
